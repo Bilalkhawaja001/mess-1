ab17014d919e7d7238225890bf2aff47e576003b
Bilal Khawaja <imbilalkhawaja@gmail.com>
2026-04-25 17:20:01 +0500

    fix sidebar gap collapse and font sizing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     @stack('styles')
-    <link rel="stylesheet" href="{{ asset('branding/nodesky-theme.css') }}?v=rollback-v3">
+    <link rel="stylesheet" href="{{ asset('branding/nodesky-theme.css') }}?v=fix-v4-sidebar">
 </head>
 <body>
 <div class="app-shell" id="appShell">
